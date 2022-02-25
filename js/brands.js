235c85bc2c538d24bd0616c9ce765cb75513eaf2
aikiryu <aikiryu1228@naver.com>
2022-02-25 18:01:59 +0900

    making brands pages

--- js/brands.js
+++ js/brands.js
@@ -1,1 +1,7 @@
-$('.view').slick();
+$('.view').slick({
+    'dots': true,
+    'customPaging': function(slick,index) {
+        var targetImage = slick.$slides.eq(index).find('img').attr('src');
+        return '<img src=" ' + targetImage + ' "/>';
+    }
+});
